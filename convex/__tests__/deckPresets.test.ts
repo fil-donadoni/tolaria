49e6f920aec13a8ebcd933910bbcf4c00d83dba9
Filippo <fil@filippodonadoni.it>
2026-04-19 12:37:16 +0200

    deck presets

---
+++ convex/__tests__/deckPresets.test.ts
@@ -0,0 +1,34 @@
+import { describe, it, expect } from "vitest";
+import { PRESET_DECKS } from "../deckPresets";
+import { getCardById } from "../cards";
+
+describe("PRESET_DECKS", () => {
+    it.each(PRESET_DECKS.map((d) => [d.name, d]))(
+        "%s has 40 cards with 16 lands (40%%)",
+        (_name, deck) => {
+            expect(deck.cards).toHaveLength(40);
+            const landCount = deck.cards.filter((c) =>
+                getCardById(c.cardId).types.includes("Land")
+            ).length;
+            expect(landCount).toBe(16);
+        }
+    );
+
+    it.each(PRESET_DECKS.map((d) => [d.name, d]))(
+        "%s only contains cards present in the registry",
+        (_name, deck) => {
+            for (const { cardId } of deck.cards) {
+                expect(() => getCardById(cardId)).not.toThrow();
+            }
+        }
+    );
+
+    it.each(PRESET_DECKS.map((d) => [d.name, d]))(
+        "%s cardName matches the registry name",
+        (_name, deck) => {
+            for (const { cardId, cardName } of deck.cards) {
+                expect(getCardById(cardId).name).toBe(cardName);
+            }
+        }
+    );
+});
--- convex/deckPresets.ts
+++ convex/deckPresets.ts
@@ -16,6 +16,9 @@ function times(n: number, cardId: string, cardName: string): DeckCard[] {
     return Array.from({ length: n }, () => ({ cardId, cardName }));
 }
 
+// All preset decks: 40 cards total, 16 lands (40%). Only cards implemented in
+// convex/cards/sets/lea.ts.
+
 const whiteWeenie: DeckPreset = {
     presetId: "white-weenie",
     name: "White Weenie",
@@ -23,17 +26,19 @@ const whiteWeenie: DeckPreset = {
     description: "Small creatures backed by removal.",
     colors: ["W"],
     cards: [
-        ...times(1, "b0faa7f2-b547-42c4-a810-839da50dadfe", "Black Lotus"),
-        ...times(1, "8ebe4be7-e12a-4596-a899-fbd5b152e879", "Mox Pearl"),
-        ...times(1, "82da0972-b17b-4600-9efd-e9430a0db04b", "Mox Sapphire"),
-        ...times(1, "92bcd1ce-19b1-4d78-8b09-95242ca08d76", "Mox Jet"),
-        ...times(1, "8945585f-4773-493d-a0fe-d707db910b38", "Mox Ruby"),
-        ...times(1, "b0e1427c-05cd-465b-be59-97ed6e39f7ba", "Mox Emerald"),
         ...times(4, "d05b92bd-797e-413f-a8b0-32e0937a1ee0", "Savannah Lions"),
+        ...times(4, "6daf1aab-1e58-4a5a-bc66-cb3f7c86e0e8", "Pearled Unicorn"),
         ...times(2, "99ec4723-b36c-4015-b361-736a6523e8f5", "Wall of Swords"),
-        ...times(3, "2722d7e2-61c6-4934-9c21-875ee78fd06c", "Disenchant"),
         ...times(4, "f8ac5006-91bd-4803-93da-f87cf196dd2f", "Serra Angel"),
-        ...times(6, "b1623d57-4729-4796-b3f7-f1837a05c6ed", "Plains"),
+        ...times(3, "2722d7e2-61c6-4934-9c21-875ee78fd06c", "Disenchant"),
+        ...times(
+            3,
+            "386ea9eb-abc1-4862-aa2d-8fb808d79490",
+            "Swords to Plowshares"
+        ),
+        ...times(2, "a2788d69-6a3a-42f0-8736-cc6b57755ecd", "Wrath of God"),
+        ...times(2, "b0da8d56-3178-44c2-9344-95d2346d326f", "Castle"),
+        ...times(16, "b1623d57-4729-4796-b3f7-f1837a05c6ed", "Plains"),
     ],
 };
 
@@ -50,8 +55,8 @@ const monoRedBurn: DeckPreset = {
             "b4eb3db3-6a7c-488a-9433-d5d1d3133816",
             "Mons's Goblin Raiders"
         ),
-        ...times(4, "78a9088f-8755-47cb-aa93-51d992ccab90", "Hurloon Minotaur"),
         ...times(4, "0ddb98e8-13fe-4786-83f7-b72c56db135a", "Hill Giant"),
+        ...times(4, "78a9088f-8755-47cb-aa93-51d992ccab90", "Hurloon Minotaur"),
         ...times(2, "73ae5276-b607-4f23-a9d2-e8cc7b8e3693", "Gray Ogre"),
         ...times(
             2,
@@ -59,7 +64,8 @@ const monoRedBurn: DeckPreset = {
             "Roc of Kher Ridges"
         ),
         ...times(2, "da237992-2919-4e37-8f56-2164095f59b5", "Fire Elemental"),
-        ...times(8, "eace2c85-976c-425e-9800-5a6ccbd91b56", "Mountain"),
+        ...times(2, "b24b5864-44c0-4bc8-8705-9504f83b2c03", "Earth Elemental"),
+        ...times(16, "eace2c85-976c-425e-9800-5a6ccbd91b56", "Mountain"),
     ],
 };
 
@@ -77,7 +83,7 @@ const monoGreenStompy: DeckPreset = {
             "Birds of Paradise"
         ),
         ...times(4, "ce2d603a-3231-4a8c-bf39-1617586ea870", "Grizzly Bears"),
-        ...times(4, "6d929c38-91e6-457c-937a-d1884f4bba44", "Scryb Sprites"),
+        ...times(2, "6d929c38-91e6-457c-937a-d1884f4bba44", "Scryb Sprites"),
         ...times(2, "8df80424-3bd9-4982-ad79-e55d9ba3b43d", "Wall of Wood"),
         ...times(
             2,
@@ -87,7 +93,7 @@ const monoGreenStompy: DeckPreset = {
         ...times(4, "c8d6081e-f686-4263-a0a2-21c0d9af5fdb", "War Mammoth"),
         ...times(2, "bfed1a95-bd67-4e16-a781-81866028af2f", "Craw Wurm"),
         ...times(2, "367dbefe-3366-408e-9fcf-7dc00f8cc201", "Giant Growth"),
-        ...times(4, "6f1c8cb0-38eb-408b-94e8-16db83999b3b", "Forest"),
+        ...times(16, "6f1c8cb0-38eb-408b-94e8-16db83999b3b", "Forest"),
     ],
 };
 
