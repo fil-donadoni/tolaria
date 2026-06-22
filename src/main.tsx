b63420b2d59ee7af800b8d420b73acccc7b44967
Filippo <fil@filippodonadoni.it>
2026-06-22 17:25:43 +0200

    feat: integrate Sentry for error tracking and logging

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,19 @@ import { ConvexReactClient } from "convex/react";
 import { ConvexAuthProvider } from "@convex-dev/auth/react";
 import { TooltipProvider } from "~/components/ui/tooltip";
 import { AppRouter } from "./router";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+    dsn: "https://82a4e88a462f5637f13141dc3b7a37d9@o4505113193218048.ingest.us.sentry.io/4511609765691393",
+    dataCollection: {
+        // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
+        // https://docs.sentry.io/platforms/javascript/guides/react/configuration/options/#dataCollection
+        userInfo: false,
+        httpBodies: [],
+    },
+    // Enable logs to be sent to Sentry
+    enableLogs: true,
+});
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL);
 
